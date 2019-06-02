083bec22c8469d3dc7cb5defad20aec3bf47c9b4
Walle <gingularity@163.com>
2019-06-02 17:23:53 +0800

    提交到了 pip  可以使用pip安装

---
+++ Python/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Author:Wa-llee
+
+from setuptools import setup,find_packages
+
+setup(
+    name='chinese2digits',
+    version= '1.0',
+    description=(
+        """最好的汉字数字(中文数字)-阿拉伯数字转换工具。包含"点二八"，"负百分之四十"等众多汉语表达方法。NLP，机器人工程必备！ The Best Tool of Chinese Number to Digits"""
+    ),
+    author='Wa-llee',
+    author_email='xrli_office@foxmail.com',
+    license='Apache License 2.0',
+    packages= find_packages(),
+    platforms=["all"],
+    url='https://github.com/Wall-ee/chinese2digits'
+)
