a4ff98a22d1a3a6f9bb70a2aff1e81d1781a7ea9
LXR <xrli_office@foxmail.com>
2019-10-31 15:35:48 +0800

    重新制作打包文件 解决 import 问题

--- Python/setup.py
+++ Python/setup.py
@@ -3,13 +3,20 @@
 # Author:Wa-llee
 
 from setuptools import setup,find_packages
-
+import os
 setup(
     name='chinese2digits',
-    version= '1.0.1',
+    version= '1.1.0',
+    keywords = ("pip", "NLP","chinese","digits_number","number_transform"),
     description=(
         """最好的汉字数字(中文数字)-阿拉伯数字转换工具。包含"点二八"，"负百分之四十"等众多汉语表达方法。NLP，机器人工程必备！ The Best Tool of Chinese Number to Digits"""
     ),
+    long_description=open(
+        os.path.join(
+            os.path.dirname(__file__),
+            'README.rst'
+        )
+    ).read(),
     author='Wa-llee',
     author_email='xrli_office@foxmail.com',
     license='Apache License 2.0',
@@ -17,3 +24,4 @@ setup(
     platforms=["all"],
     url='https://github.com/Wall-ee/chinese2digits'
 )
+
