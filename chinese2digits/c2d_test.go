560b1af0ea06a02fbc3fa8682183626c0f3848c1
lixiaoran <xrli_office@foxmail.com>
2020-10-27 14:25:08 +0800

    add main func for testing

--- chinese2digits/c2d_test.go
+++ chinese2digits/c2d_test.go
@@ -47,3 +47,18 @@ func TestSplit(t *testing.T) { // 测试函数名必须以Test开头，必须接
 		})
 	}
 }
+
+// func main() {
+// 	fmt.Println("输入：负百分之点二八你好啊三五四百分之三五是不是点伍零百分之负六十五点二八")
+// 	fmt.Println("输出：TakeChineseNumberFromString 方法")
+// 	fmt.Println(chinese2digits.TakeNumberFromString("百分之5负千分之15"))
+// 	fmt.Println(chinese2digits.TakeNumberFromString("三十万"))
+// 	fmt.Println(chinese2digits.TakeNumberFromString("十万啦啦啦300万nihao400十五点八"))
+// 	fmt.Println(chinese2digits.TakeChineseNumberFromString("百分之四百三十二万分之四三千分之五"))
+// 	fmt.Println(chinese2digits.TakeChineseNumberFromString("llalala万三威风威风千四五", true, false))
+// 	fmt.Println(chinese2digits.TakeChineseNumberFromString("负百分之点二八你好啊三五四百分之三五是不是点五零百分之负六十五点二八百分之四十啦啦啦啊四万三千四百二"))
+// 	//自动判断转换方式，使用正则引擎
+// 	fmt.Println(chinese2digits.TakeChineseNumberFromString("壹亿叁仟伍佰万你好亿万", "auto", true, true))
+// 	fmt.Println(chinese2digits.TakeChineseNumberFromString("一千八百万啦啦啦四万七,皮皮四千万十七", true, true, true, false))
+// 	fmt.Println(chinese2digits.TakeChineseNumberFromString("今天万科怎么样负点三六姹紫嫣红千千万万", nil, true, true, false))
+// }
